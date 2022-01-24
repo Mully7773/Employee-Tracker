74522ef51ecc1c5f843de80869e59590f532d048
Nick M <mully7773@gmail.com>
2022-01-24 16:21:43 -0600

    Add schema.sql

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE IF EXISTS employeeManagement_db;
+CREATE DATABASE employeeManagement_db;
+
+USE employeeManagement_db;
+
+CREATE TABLE department 
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE role
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee
+(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id),
+    FOREIGN KEY (manager_id) 
+    REFERENCES employee(id)
+);
